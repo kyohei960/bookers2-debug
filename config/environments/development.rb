da9dcf7b5869e6b7bf0acd466a78957fe5ae8100
Kyohei Sasaki <106052021+kyohei960@users.noreply.github.com>
2022-07-10 14:38:27 +0000

    [Update]メールフォーム作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,17 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "8d9da48383b343029dba991e43a0c2d2.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            'メールアドレス',
+    password:             'パスワード',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 end
